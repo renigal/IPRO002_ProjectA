96b81a403571332d37b88c618bf08f1187b1dcb0
renigal <166690549+renigal@users.noreply.github.com>
2024-04-21 22:25:56 +1000

    Update Login.java

--- Login.java
+++ Login.java
@@ -7,9 +7,10 @@ public class Login {
         // Initialize the user credentials map
         userCredentials = new HashMap<>();
         // Add some default users (username, password)
-        userCredentials.put("student1", "password1");
-        userCredentials.put("student2", "password2");
-        userCredentials.put("staff1", "password3");
+        userCredentials.put("student1", "pass1");
+        userCredentials.put("student2", "pass2");
+        userCredentials.put("student3", "pass3");
+        userCredentials.put("staff", "admin");
     }
 
     public boolean authenticate(String username, String password) {
